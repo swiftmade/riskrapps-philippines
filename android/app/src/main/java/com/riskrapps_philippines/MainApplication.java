3adb6c8c5c84e9ea7f6f545b0f0a78c3d4bff366
Ahmet Özışık <ozisikahmet@gmail.com>
2019-05-13 17:37:46 +0300

    add ssas-app-core and update package.json

--- android/app/src/main/java/com/riskrapps_philippines/MainApplication.java
+++ android/app/src/main/java/com/riskrapps_philippines/MainApplication.java
@@ -3,7 +3,6 @@ package com.riskrapps_philippines;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblongmana.webviewfileuploadandroid.AndroidWebViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.rnfs.RNFSPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new AndroidWebViewPackage(),
             new VectorIconsPackage(),
             new RNFSPackage()
       );
